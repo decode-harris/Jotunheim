76de6e5b3fbf1d3b88b66418196af44513ef59a9
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-08-03 22:51:56 +1000

    Application Data-Flow
    Created a data-flow application state function for selecting & navigating to specific views.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 /*
-    app functions & properties
+    Application Functions & Properties
 
     storageAvailable
         1. Set a storage variable
@@ -60,19 +60,188 @@ else {
     console.log('no storage available');
 }
 
-// select the button [ open ] on the unordered list element
-let open = document.querySelector('.open'); // change to new-button on final build
+// declare the application view states
+const home = document.querySelector('#home');
+const load = document.querySelector('#load');
 
-// click event [ open ] : open the form
-open.addEventListener('click', ()=> {
+// function [ applicationDefaults ] : remove all states from application view
+applicationDefaults = () => {
+    // test route
+    console.log('[ applicationDefaults ] : remove all states from application view');
 
+    // apply the [ default property ] value to all application states
+    home.style.display = 'none';
+    load.style.display = 'none';
+    form.style.display = 'none';
+    list.style.display = 'none';
+};
+
+// function [ homepageScreen ] : reactive display the homepage state view 
+homepageScreen = () => {
+    // test route
+    console.log('[ developerControls ] ==> [ homepageScreen ] : display homepage view state to current user');
+
+    // assign the homepage view to display : flex
+    home.style.display = 'flex';
+};
+
+// function [ loadingScreen ] : display the loading screen element between click events
+loadingScreen = () => {
+
+    // test route
+    console.log('[ startButton ] ==> [ loadingScreen ]');
+
+    load.style.display = 'flex';
+
+    // delay init function [ displayForm ] by 2 seconds
+    setTimeout(() => {
+
+        // init function [ applicationDefaults ] : clear application state view
+        applicationDefaults();
+
+        // init function [ displayForm ] : data flow [ loadingScreen ] ==> [ displayForm ]
+        displayForm();
+
+    }, 2000);
+}
+
+// button selector [ start button ]
+let startButton = document.querySelector('#startButton');
+
+// click event [ start button ]
+startButton.addEventListener('click', ()=> {
+
+    // test
+    console.log('start button clicked');
+
+    // init function [ applicationDefaults ] : clear application state view
+    applicationDefaults();
+
+    // init function [ loadingScreen]
+    loadingScreen();
+});
+
+/*
+    developer controls
+
+    call on-load to run specific functions or view states
+
+    create singular application states for testing
+
+    can add or remove certain functions with ease
+
+    makes for an easier way to debug function error or 
+
+    invalid data processing.
+
+*/
+developerControls = () => {
+
+    // init functions
+    applicationDefaults();
+    homepageScreen();
+    
+}
+
+// init function [ developerControls ]
+developerControls();
+
+
+
+/*
+    new form button & show list button
+
+    refactor this click events as well as providing button
+
+    states within the application. Add the buttons inside 
+
+    a button container & display during certain stages
+
+    of the application.
+
+*/
+
+// function [ buttonControls ]
+buttonControls = () => {
+
+    // test function
+    console.log('[ buttonControls ] : active');
+
+    // select the buttons global container
+    let buttons = document.querySelector('#list .buttons');
+    console.log(buttons);
+
+    // validate if [ form ] is active
+    if (form.style.display === 'flex') {
+
+        // test validation
+        console.log('[ buttonControls ] : form = flex');
+
+        return
+    }
+    else if (list.style.display === 'flex') {
+
+        // test validation
+        console.log('[ buttonControls ] : list = flex');
+
+        // set actions buttons container
+        // actionButtons.style.display = 'flex';
+        // operationButtons.style.display = 'none';
+
+        // buttons.appendChild(actionButtons);
+
+
+        return
+    }
+    // else if (active.style.display === 'flex') {
+
+    //     // set operation buttons container
+
+    //     return
+    // }
+    else {
+
+        console.log('[ buttonControls ] : validated');
+    }
+
+};
+
+// select the button containers
+let action = document.querySelector('#actionButtons');
+let operation = document.querySelector('#operationButtons');
+
+buttonDefaults = () => {
+    
+    // init function [ buttonControls ]
+    buttonControls();
+
+};
+
+// init function [ buttonDefaults ]
+buttonDefaults();
+
+
+// function [ actionButtons ] : display actions container, remove operations container
+actionButtons = () => {
+    console.log('[ actionButtons ] : active'); // test function properties
+    action.style.display = 'flex';
+    operation.style.display = 'none';
+}
+// function [ operationButtons ] : display operations container, remove actions container
+operationButtons = () => {
+    console.log('[ operationButtons ] : active'); // test function properties
+    action.style.display = 'none';
+    operation.style.display = 'flex';
+}
+
+// click event [ new list button ] : open a new form
+newButton.addEventListener('click', ()=> {
     // display the form
     form.style.display = 'flex';
     // hide the list
     list.style.display = 'none';
     // display the show button
     show.style.display = 'block';
-
 });
 
 // select the button [ show ] on the user form element
