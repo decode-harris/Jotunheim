7940575f027f21dbd34b5c5edb01d12ec3100126
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-08-13 18:29:06 +1000

    Update States,js
    Validate length of created items. Apply new fixed styles to the button container element.

--- src/js/states.js
+++ src/js/states.js
@@ -20,7 +20,15 @@ checkState = () => {
     let items = document.querySelectorAll('.list .items');
 
     // check the number of items created
-    if (items.length > 2) {
+    if (items.length > 2 && items.length < 4) {
+
+        // set list style display as flex [ default display properties ]
+        list.style.display = 'flex';
+
+        // test 
+        console.log('[ check state ] : items length between 2 & 4');
+    }
+    else if (items.length > 4) {
 
         // set grid properties for unordered list [ list ] element
         list.style.display = 'grid';
@@ -28,11 +36,26 @@ checkState = () => {
         list.style.gridAutoRows = 'minmax(100px, auto)';
         list.style.gap = '10px';
         list.style.padding = '10px';
+        list.style.overflow = 'scroll';
 
-        // test 
-        console.log('grid properties applied to list element');
-        
-        return
+        let lastItem = list.lastChild;
+        lastItem.style.marginBottom = '10vh';
+
+        // test last item
+        console.log('last list item : ' + lastItem);
+
+        // test length parameter
+        console.log('[ check state ] : items length = ' + items.length);
+
+        let buttonsContainer = list.querySelector('.buttons');
+
+        console.log('[ checkstate ] : item length greater than 4 units || test : ' + actionButtons);
+
+        // apply new absolute position properties to action buttons container
+        buttonsContainer.style.position = 'fixed';
+        buttonsContainer.style.bottom = '0';
+        buttonsContainer.style.left = '0';
+        buttonsContainer.style.zIndex = '400';
     }
     else {
         // set list style display as flex
